0eb7f92f6c14aeaa90469f5c383d9b99c275fad1
Hyunseung Do <gustmd1210@gmail.com>
2018-04-10 03:51:53 -0400

    Setup initial layout and stuff.

--- app/src/main/java/edu/temple/cis3515lab8/MainActivity.java
+++ app/src/main/java/edu/temple/cis3515lab8/MainActivity.java
@@ -1,13 +1,21 @@
 package edu.temple.cis3515lab8;
 
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    WebPageAdapter webPageAdapter;
+    ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        webPageAdapter = new WebPageAdapter(getSupportFragmentManager());
+        viewPager = findViewById(R.id.viewPager);
+        viewPager.setAdapter(webPageAdapter);
     }
 }
+
---
+++ app/src/main/java/edu/temple/cis3515lab8/WebPageAdapter.java
@@ -0,0 +1,28 @@
+package edu.temple.cis3515lab8;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+/**
+ * Created by Steve on 4/10/2018.
+ */
+
+public class WebPageAdapter extends FragmentStatePagerAdapter {
+
+    public WebPageAdapter(FragmentManager fm){
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        Fragment fragment = new WebFragment();
+        return fragment;
+    }
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+}
